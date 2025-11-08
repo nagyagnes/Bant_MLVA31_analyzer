a4bbcf998429866d212a791b361d7f5dc2565395
Agnes Nagy <152724387+nagyagnes@users.noreply.github.com>
2025-11-08 21:42:54 +0100

    corrected package loading

--- Script_directory/MLVA_distmatrix_v3_modMatchCoeffwoNA_closestrelative.R
+++ Script_directory/MLVA_distmatrix_v3_modMatchCoeffwoNA_closestrelative.R
@@ -10,11 +10,10 @@
 
 # Loading required packages:
 
-library(permute, lib.loc="/usr/lib/R/library/")
-library(lattice, lib.loc="/usr/lib/R/library/")
-library(vegan, lib.loc="/usr/lib/R/library/")
-library(ape, lib.loc="/usr/lib/R/library/")
-library(cluster, lib.loc="/usr/lib/R/library/")
+library(permute)
+library(lattice)
+library(vegan)
+library(cluster)
 
 ################################################
 ### SET WORKING DIRECTORY for "workdir3.txt" ###
