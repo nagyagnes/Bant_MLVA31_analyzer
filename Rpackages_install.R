5efa6e44f2954eb1da44bc00523074c75564d651
Agnes Nagy <152724387+nagyagnes@users.noreply.github.com>
2025-11-08 18:25:14 +0100

    bugfixes

--- Rpackages_install.R
+++ Rpackages_install.R
@@ -2,27 +2,23 @@
 
 # This script is part of Bant_MLVA31_analyzer pipeline installed with apptainer
 
-myPaths <- .libPaths()
-myPaths <- c(myPaths, '/usr/lib/R/library/')
-.libPaths(myPaths)  # add new path 
-
 if(!require(permute)){
-  install.packages("permute")
-  library(permute)
+  install.packages("permute", lib="/usr/lib/R/library/")
+  library(permute, lib.loc="/usr/lib/R/library/")
 }
 if(!require(lattice)){
-  install.packages("lattice")
-  library(lattice)
+  install.packages("lattice", lib="/usr/lib/R/library/")
+  library(lattice, lib.loc="/usr/lib/R/library/")
 }
 if(!require(vegan)){
-  install.packages("vegan")
-  library(vegan)
+  install.packages("vegan", lib="/usr/lib/R/library/")
+  library(vegan, lib.loc="/usr/lib/R/library/")
 }
 if(!require(ape)){
-  install.packages("ape")
-  library(ape)
+  install.packages("ape", lib="/usr/lib/R/library/")
+  library(ape, lib.loc="/usr/lib/R/library/")
 }
 if(!require(cluster)){
-  install.packages("cluster")
-  library(cluster)
+  install.packages("cluster", lib="/usr/lib/R/library/")
+  library(cluster, lib.loc="/usr/lib/R/library/")
 }
