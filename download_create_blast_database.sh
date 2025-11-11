358a2481715988df911f5636504c851c8cab9d41
Agnes Nagy <152724387+nagyagnes@users.noreply.github.com>
2025-11-11 11:11:04 +0100

    update

--- download_create_blast_database.sh
+++ download_create_blast_database.sh
@@ -5,6 +5,7 @@
 path=$(realpath ./)
 DIR_PATH=$(realpath ./)
 patterndir=$DIR_PATH/Supporting_files
+repeats=("bams01" "bams03" "bams05" "bams13" "bams15" "bams21" "bams22" "bams23" "bams24" "bams25" "bams28" "bams30" "bams31" "bams34" "bams44" "bams51" "bams53" "CG3" "pXO1" "pXO2" "vntr12" "vntr16" "vntr17" "vntr19" "vntr23" "vntr35" "vrrA" "vrrB1" "vrrB2" "vrrC1" "vrrC2")
 
 cd $patterndir
 mkdir $patterndir/Blast_dbase
@@ -12,3 +13,8 @@ cd $patterndir/Blast_dbase
 wget -c https://zenodo.org/records/17078231/files/Bcereus_genomes_v2.fasta.gz
 gzip -d *.gz
 makeblastdb -in Bcereus_genomes_v2.fasta -dbtype nucl -parse_seqids -out Bcereus_genomes -title "Bacillus cereus group genomes"
+cd $patterndir
+for k in ${!repeats[@]}; do
+	minimap2 -d ${repeats[$k]}_ref.mmi ${repeats[$k]}_ref.fa
+	samtools faidx ${repeats[$k]}_ref.fa
+done
