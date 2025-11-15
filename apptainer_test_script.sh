67c751558d642fc2ebb7bdce771f02084c0d4421
Agnes Nagy <152724387+nagyagnes@users.noreply.github.com>
2025-11-15 11:32:09 +0100

    bugfixes

--- apptainer_test_script.sh
+++ apptainer_test_script.sh
@@ -5,7 +5,7 @@
 path=$(realpath ./)
 DIR_PATH=$(realpath ./)
 
-./Bant_MLVA31_analyzer.sh -p /opt/Bant_MLVA31_analyzer/Supporting_files -d /opt/Bant_MLVA31_analyzer/Script_directory -s Test_1,Test_2 -o $DIR_PATH/Demo_files -m Y -i N
+./Bant_MLVA31_analyzer.sh -s Test_1,Test_2 -o $DIR_PATH/Demo_files -m Y -i N
 mv {$DIR_PATH/Demo_files/Test_1/Test_1.fastq,$DIR_PATH/Demo_files/Test_2/Test_2.fastq} $DIR_PATH/Demo_files
 awk -F, 'NR==FNR { keys[$1]=1; next } !($1 in keys) { print $0 }' $DIR_PATH/Demo_files/Demo_results/Test_1/Test_1_final_results.csv $DIR_PATH/Demo_files/Test_1/Results/Test_1_final_results.csv > $DIR_PATH/Demo_files/Comparison_Test_1_results.txt
 awk -F, 'NR==FNR { keys[$1]=1; next } !($1 in keys) { print $0 }' $DIR_PATH/Demo_files/Demo_results/Test_2/Test_2_final_results.csv $DIR_PATH/Demo_files/Test_2/Results/Test_2_final_results.csv > $DIR_PATH/Demo_files/Comparison_Test_2_results.txt
