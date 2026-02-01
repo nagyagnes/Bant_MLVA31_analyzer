586559baae3d5cfe257e6a32892cdcd710d5691f
Agnes Nagy <152724387+nagyagnes@users.noreply.github.com>
2026-02-01 11:03:49 +0100

    updated database

--- download_create_blast_database.sh
+++ download_create_blast_database.sh
@@ -9,6 +9,6 @@ patterndir=$DIR_PATH/Supporting_files
 cd $patterndir
 mkdir $patterndir/Blast_dbase
 cd $patterndir/Blast_dbase
-wget -c https://zenodo.org/records/17078231/files/Bcereus_genomes_v2.fasta.gz
+wget -c https://zenodo.org/records/18399429/files/Bcereus_genomes_v3.fasta.gz
 gzip -d *.gz
-makeblastdb -in Bcereus_genomes_v2.fasta -dbtype nucl -parse_seqids -out Bcereus_genomes -title "Bacillus cereus group genomes"
+makeblastdb -in Bcereus_genomes_v3.fasta -dbtype nucl -parse_seqids -out Bcereus_genomes -title "Bacillus cereus group genomes"
